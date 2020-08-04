c88cdfba954575274e9826b13f299e4558774735
missnyasha <rungwandinyasha@gmail.com>
2020-08-04 23:56:58 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -7,26 +7,51 @@ using namespace std;
 int main()
 {
 	auto myScreen = Screen{6,6};
+
+	myScreen.move(1,1);
+	myScreen.set('*');
+	myScreen.move(2,1);
+	myScreen.set('*');
+	myScreen.forward();
+	myScreen.set('*');
+	myScreen.move(3,1);
+	myScreen.set('*');
+	myScreen.forward();
+	myScreen.forward();
+	myScreen.set('*');
+	myScreen.move(4,1);
+	myScreen.set('*');
+	myScreen.forward();
+	myScreen.forward();
 	myScreen.forward();
 	myScreen.set('*');
 	myScreen.down();
+	myScreen.forward();
 	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
+	myScreen.down();
+	myScreen.forward();
+	myScreen.set('*');
+	myScreen.move(5,1);
+	myScreen.set('*');
+	myScreen.move(6,1);
+	myScreen.set('*');
+	myScreen.move(1,6);
+	myScreen.set('*');
+	myScreen.move(2,6);
+	myScreen.set('*');
+	myScreen.move(3,6);
+	myScreen.set('*');
+	myScreen.move(4,6);
+	myScreen.set('*');
+	myScreen.move(5,6);
+	myScreen.set('*');
+
+
 
 	myScreen.display();
+
+
 	cout << endl;
-
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
-
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
-	myScreen.display();
-
 	return 0;
 }
 
