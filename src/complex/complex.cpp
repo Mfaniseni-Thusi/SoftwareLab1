c3c348c5274dd257d733ab9c3a7daef4879b02f8
Mfaniseni-Thusi <1229215@students.wits.ac.za>
2020-08-04 23:42:40 +0200

    Exercise 3.2

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -6,22 +6,20 @@
 
 using namespace std; // cout, endl, complex are all part of this namespace
 
-int main()
-{
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+int main(){
+    using complexType = complex<float>; // Used aliasing HERE
+    complexType num1{ 2.0, 2.0 }; // using C++11 uniform initialisation syntax
+    complexType num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
+    auto answer = num1 * num2; // using C++11 auto keyword,
+    // answer is of type: complex<float>
 
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
-
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real()
-			<< " + " << answer.imag() << "j"
-			<< endl	<< endl;
-
-	// answer++;
-
-	return 0;
+    cout << "The answer is: " << answer << endl;
+    cout << "Or in a more familiar form: " << answer.real() << " + " <<
+    answer.imag() << "j" << endl;
+    // answer++;
+    return 0;
 }
+
 
 //the commented line will not work because it is gcc compiler
 // ignores every text that comes after "//" in c++
