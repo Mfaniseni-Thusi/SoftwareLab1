24a2c3896b3ba3fe92b261cdd8f1af2d456e1120
Mfaniseni-Thusi <1229215@students.wits.ac.za>
2020-08-04 23:43:33 +0200

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -1,24 +1,64 @@
-// complex.cpp
-// Multiplying complex numbers
+#include <iostream>
+#include <complex>
+#include <cmath>
+#include <math.h>
+#include <string>
 
-#include <iostream>	// contains the definition of cout, endl
-#include <complex>	// contains the complex class definition
+using namespace std;
 
-using namespace std; // cout, endl, complex are all part of this namespace
+int main()
+{
+    char ny;
 
-int main(){
-    using complexType = complex<float>; // Used aliasing HERE
-    complexType num1{ 2.0, 2.0 }; // using C++11 uniform initialisation syntax
-    complexType num2{ 4.0, -2.0 }; // old syntax: complex<float> num2(4.0,-2.0);
-    auto answer = num1 * num2; // using C++11 auto keyword,
-    // answer is of type: complex<float>
+    cout << "Press y to calculate or q to quit!" << endl;
 
-    cout << "The answer is: " << answer << endl;
-    cout << "Or in a more familiar form: " << answer.real() << " + " <<
-    answer.imag() << "j" << endl;
-    // answer++;
+    while(cin >> ny)
+    {
+        if(ny == 'y'){
+            cout<<"enter the values"<<endl;
+            int a,b,c;
+            cin >> a;
+            cin >> b;
+            cin >> c;
+
+            double prod1=4*a*c;
+            double prod2=2*a;
+            double power= pow (b, 2);
+            double diff = power - prod1;
+
+
+            if(diff<0)
+            {
+                complex<double> num(diff,0);
+                complex<double> result= sqrt (num);
+                double imagpart = result.imag();
+                double top1= (-1*b)/ prod2;
+                cout<< "x = "<<top1<< " + "<<imagpart/prod2<<"j"<<" or "<< "x ="<<top1<<" - "<<imagpart/prod2<<"j"<<endl;
+            }
+
+            else
+            {
+                double square=sqrt(diff);
+                double sum=((-1*b)+square)/prod2;
+                double dif=((-1*b)-square)/prod2;
+                cout << "x = " << sum << " or " << dif << endl;
+            }
+            cout << "Press y to calculate again or q to quit!" << endl;
+            cin >> ny;
+
+            if(ny == 'q')
+            {
+                break;
+            }
+            break;
+
+        } else if(ny == 'q'){
+            break;
+        }
+    }
     return 0;
 }
+
 
 
 //the commented line will not work because it is gcc compiler
